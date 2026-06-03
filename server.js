0e15d2dd7638802ad6e971115b2e0cc8977ad5a6
Demi1Codex <demian.parra.crg2020@gmail.com>
2026-06-03 12:00:48 -0400

    Configurar deploy en Render.com

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/removevote', (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`
   🚀 SERVIDOR GEOBAR ACTIVO
